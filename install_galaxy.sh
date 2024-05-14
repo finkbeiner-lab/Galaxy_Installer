b72dcb642073b50baaa871978c05cdabc4c1324f
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-05-14 14:43:43 -0700

    Made so Galaxy startup runs in the background but tails the log in the foreground Made a cleanup function to gracefully shutdown Galaxy, but kill zombies, and leave the logfile around if we've hit an error

--- install_galaxy.sh
+++ install_galaxy.sh
@@ -23,39 +23,48 @@ cd "$galaxy_dir"
 
 echo "Starting up Galaxy..."
 # Start Galaxy in the background
-nohup ./run.sh --daemon &
-tail -F nohup.out &
+nohup ./run.sh --daemon &> install_galaxy.log &
+nohup_pid=$!
+tail -f install_galaxy.log &
 sleep 5
 
 # Function to check if Galaxy is up by querying the main page
 check_galaxy() {
     echo "Checking if Galaxy is up..."
-    for i in {1..600}; do  # Check for up to 10 minutes
+    for i in {1..300}; do  # Check for up to 10 minutes
         echo "Attempting to reach Galaxy server..."
         if curl -s http://localhost:8080 | grep -q 'Galaxy'; then
             return 0
         fi
-        sleep 1
+        sleep 2
     done
     return 1
 }
 
+# Function to shutdown Galaxy
+shutdown_galaxy() {
+    echo "Shutting down Galaxy..."
+    ./run.sh --stop-daemon
+    sleep 5 # Give Galaxy some time to shut down gracefully
+    kill $nohup_pid &> /dev/null # No zombies.
+    return 0
+}
+
 # Verify Galaxy startup
 if check_galaxy; then
-    echo "Galaxy setup complete. Server is up and running."
+    echo "Galaxy setup complete. Server is up and responsive."
 else
-    echo "Error: Galaxy server did not start successfully."
-    ./run.sh --stop-daemon
-    rm nohup.out
+    echo "☹️ Error: Galaxy server did not start successfully.  Take a peak at $galaxy_dir/install_galaxy.log"
+    shutdown_galaxy
+    sync # Dump any remaining log lines from tail to the console
     popd
     exit 1
 fi
 
-echo "Shutting down Galaxy..."
-./run.sh --stop-daemon
+shutdown_galaxy
 
-# Clean
-rm nohup.out
+echo "Cleaning up..."
+rm install_galaxy.log
 
 # Return to the original directory without changing the user's working directory
 popd
