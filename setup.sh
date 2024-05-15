723a1f7c16de892f109075d733dccd1417739f55
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-05-15 13:02:43 -0700

    Added placeholder scripts for the next couple of tasks

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 echo "Installing Galaxy and its dependencies on macOS..."
 
 # Ensure each script is executable
-chmod +x install_xcode_tools.sh install_homebrew.sh install_zsh.sh install_oh-my-zsh.sh install_python_3.sh install_galaxy.sh
+chmod +x install_xcode_tools.sh install_homebrew.sh install_zsh.sh install_oh-my-zsh.sh install_python_3.sh configure_git.sh install_galaxy.sh install_tool_shed.sh
 
 ./install_xcode_tools.sh
 ./install_homebrew.sh
 ./install_zsh.sh
 ./install_oh-my-zsh.sh
 ./install_python_3.sh
+./configure_git.sh
 ./install_galaxy.sh
+./install_tool_shed.sh
 
 echo "Setup completed successfully."
