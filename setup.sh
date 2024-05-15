70d4e2add51546bd820a4df46973b57b166d4f7a
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-05-15 14:25:38 -0700

    Added the failure condition if any of the scripts returns 1. Also chmoded the new scripts +x

--- setup.sh
+++ setup.sh
@@ -2,15 +2,35 @@
 echo "Installing Galaxy and its dependencies on macOS..."
 
 # Ensure each script is executable
-chmod +x install_xcode_tools.sh install_homebrew.sh install_zsh.sh install_oh-my-zsh.sh install_python_3.sh configure_git.sh install_galaxy.sh install_tool_shed.sh
+chmod +x \
+install_xcode_tools.sh \
+install_homebrew.sh \
+install_zsh.sh \
+install_oh-my-zsh.sh \
+install_python_3.sh \
+configure_git.sh \
+install_galaxy.sh \
+install_mercurial.sh \
+install_tool_shed.sh
 
-./install_xcode_tools.sh
-./install_homebrew.sh
-./install_zsh.sh
-./install_oh-my-zsh.sh
-./install_python_3.sh
-./configure_git.sh
-./install_galaxy.sh
-./install_tool_shed.sh
+# Function to run a script and check its exit status
+run_script() {
+    ./$1
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed."
+        exit 1
+    fi
+}
+
+# Run each script and check for success
+run_script install_xcode_tools.sh
+run_script install_homebrew.sh
+run_script install_zsh.sh
+run_script install_oh-my-zsh.sh
+run_script install_python_3.sh
+run_script configure_git.sh
+run_script install_galaxy.sh
+run_script install_mercurial.sh
+run_script install_tool_shed.sh
 
 echo "Setup completed successfully."
