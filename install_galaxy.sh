dd67384bdaf786629cbe1f83b8a9a2b2c8e92859
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2024-05-30 14:29:06 -0700

    The checking out of the tag isn't working on the remote machine Trying to make sure we're in the directory when we check it out

--- install_galaxy.sh
+++ install_galaxy.sh
@@ -14,7 +14,7 @@ pull_repo() {
         if git pull; then
             log_info "Galaxy repository update through fast-forward was successful."
         else
-            log_error "Cannot fast-forward. Your copy of Galaxy has diverged significantly from the offical repository. You'll need to resolve a merge manually, or delete $GALAXY_DIR and start fresh."
+            log_error "Cannot fast-forward. Your copy of Galaxy has diverged significantly from the official repository. You'll need to resolve a merge manually, or delete $GALAXY_DIR and start fresh."
             popd &> /dev/null
             exit 1
         fi
@@ -24,8 +24,14 @@ pull_repo() {
 # Function to get the latest tagged release matching the pattern v00.00.00, this filters out "dev" releases and other random tags
 checkout_latest_release() {
     log_info "Finding latest release..."
+    cd "$GALAXY_DIR"
     git fetch --tags
+
+    # Find the latest tag matching the pattern
     latest_tag=$(git tag -l | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)
+
+    # Log the found tag
+    log_info "Latest tag found: $latest_tag"
 
     if [ -z "$latest_tag" ]; then
         log_error "No valid release tags found. Unsure how to continue."
@@ -38,7 +44,7 @@ checkout_latest_release() {
     if git checkout "$latest_tag"; then
         log_info "Checked out latest release: $latest_tag"
     else
-        log_error "Unable to check out the latest release $latest_tag. Unsure how to continue. You'll need to resolve manually or delete $GALAXY_DIR and start fresh."
+        log_error "Unable to check out the latest release. Unsure how to continue. You'll need to resolve manually or delete $GALAXY_DIR and start fresh."
         popd &> /dev/null
         exit 1
     fi
